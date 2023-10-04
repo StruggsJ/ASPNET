5ed7f2723ce48fcd2c2d8ec918852c6442e988f5
StruggsJ <strjac5@gmail.com>
2023-10-04 14:17:21 -0500

    Finish Delete Product

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -53,5 +53,11 @@ namespace ASPNET.Controllers
             repo.InsertProduct(productToInsert);
             return RedirectToAction("Index");
         }
+
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+            return RedirectToAction("Index");
+        }
     }
 }

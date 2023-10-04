b56e15a6854e85c474f0ccf20b524a69a55b5df3
StruggsJ <strjac5@gmail.com>
2023-10-04 13:53:29 -0500

    Finished Create Product

--- Models/Product.cs
+++ Models/Product.cs
@@ -12,5 +12,7 @@
         public int CategoryID { get; set; }
         public int OnSale { get; set; } 
         public int StockLevel { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
